37b7070c241e5df9a216895b0094908383ce2257
Vezil <SzymonWojaczek@wp.pl>
2021-04-23 16:14:30 +0200

    add autobindValue decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
+//autobind decorator
+function autobindValue(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+
+            return boundFn;
+        }
+    };
+
+    return adjustedDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById(
@@ -17,7 +36,31 @@ class ProjectInput {
 
         this.element = importedNode.firstElementChild as HTMLFormElement;
 
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector(
+            '#title'
+        ) as HTMLInputElement;
+
+        this.descriptionInputElement = this.element.querySelector(
+            '#description'
+        ) as HTMLInputElement;
+
+        this.peopleInputElement = this.element.querySelector(
+            '#people'
+        ) as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    @autobindValue
+    private submitHandler(event: Event) {
+        event.preventDefault();
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
