affed62c3e32498b7143a69f1a4ad2d0ffb42ab9
Vezil <SzymonWojaczek@wp.pl>
2021-04-28 19:10:00 +0200

    init ProjectState

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+// state
+
+class ProjectState {
+    private projects: any[] = [];
+
+    addProject(title: string, description: string, numberOfPeople: number) {
+        const newProject = {
+            id: Math.random().toString(),
+            title,
+            description,
+            people: numberOfPeople
+        };
+
+        this.projects.push(newProject);
+    }
+}
+
 //validation
 
 interface Validatable {
@@ -65,6 +82,45 @@ function autobindValue(_: any, _2: string, descriptor: PropertyDescriptor) {
     };
 
     return adjustedDescriptor;
+}
+
+// ProjectList Class
+
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById(
+            'project-list'
+        )! as HTMLTemplateElement;
+
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
+
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent =
+            this.type.toUpperCase() + 'PROJECTS';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
 }
 
 // ProjectInput Class
@@ -176,3 +232,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
