3e9bf7bfa17ebf4c65f0ee7861203506dc50ed34
Vezil <SzymonWojaczek@wp.pl>
2021-04-30 21:12:37 +0200

    fix bug

--- src/app.ts
+++ src/app.ts
@@ -153,7 +153,15 @@ class ProjectList {
         this.element.id = `${this.type}-projects`;
 
         state.addListener((projects: Project[]) => {
-            this.assignedProjects = projects;
+            const relevantProjects = projects.filter((project) => {
+                if (this.type === 'active') {
+                    return project.status === ProjectStatus.Active;
+                }
+
+                return project.status === ProjectStatus.Finished;
+            });
+
+            this.assignedProjects = relevantProjects;
             this.renderProjects();
         });
 
@@ -165,6 +173,8 @@ class ProjectList {
         const listElement = document.getElementById(
             `${this.type}-projects-list`
         ) as HTMLInputElement;
+
+        listElement.innerHTML = '';
 
         for (const project of this.assignedProjects) {
             const listItem = document.createElement('li');
